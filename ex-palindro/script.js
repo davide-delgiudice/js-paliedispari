f3de25c40d60bbc9b43c8409ab42b1e36c308117
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-21 16:36:51 +0100

    [UPD] create user prompt and function

---
+++ ex-palindro/script.js
@@ -0,0 +1,26 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// utilizzare split, join e reverse
+// split ci permette di separare le parole o i caratteri
+// join ci restituisce l'array in una stringa
+// reversed ci capovolge l'ordine degli elementi
+
+// Dichiarare una variabile dove far inserire all'utente una parola
+// Dichiarare una funzione
+// Diamo un parametro alla funzione che ci servirà per la parola data
+// Usiamo split per separare i caratteri
+// Creiamo un ciclo per controllare la parola
+
+// Richiamare la funzione
+
+const userWord = prompt('Inserisci una parola');
+
+function paliWord(word){
+    let letters = userWord;
+
+    letters = word.split("");
+
+}
+
