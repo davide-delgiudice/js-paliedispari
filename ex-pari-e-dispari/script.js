039b721c4fddce3160921c7788f3fbb72d66c6bd
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-21 19:36:30 +0100

    [UPD] create user prompt variables

---
+++ ex-pari-e-dispari/script.js
@@ -0,0 +1,14 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// dichiaro una variabile di prompt per l'utente per la tipologia di numero
+// dichiaro una variabile di prompt per l'utente per il numero
+// dichiaro una funzione e la evoco
+// dichiaro una variabile che restituisca un numero randomico per il computer
+// effettuo la somma dei due valori
+// effettuo un if per stabilire se la somma dei due valori è pari o dispari
+// dichiaro in console il vincitore in base alla scelta iniziale
+
+const tipoNum = prompt('Scegli pari o dispari');
+
+const userNum = prompt('Scegli un numero da 1 a 5');
